6594f0146edcf7068d3f967ba79ea2190c452c1f
Mohamed Shebl <hamdysheblm@gmail.com>
2024-08-10 14:45:57 +0300

    add basic server add config setter

--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/Adosh74/geek-blog/config"
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+)
 
 func main() {
-	fmt.Println("Geek Blog...")
+
+	configs := configSet()
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message":  "pong",
+			"app name": viper.Get("App.Name"),
+		})
+	})
+	r.Run(fmt.Sprintf("%s:%s", configs.Server.Host, configs.Server.Port))
+}
+
+func configSet() config.Config {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("config")
+
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Printf("Error reading config file, %s", err)
+	}
+
+	var configs config.Config
+
+	err := viper.Unmarshal(&configs)
+	if err != nil {
+		fmt.Printf("unable to decode into struct, %v", err)
+	}
+
+	return configs
 }
