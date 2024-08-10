2b6cf493b5bbe8198899d71a5556ed617b4456e8
Mohamed Shebl <hamdysheblm@gmail.com>
2024-08-10 15:24:22 +0300

    update main ro execute cmd argument

--- main.go
+++ main.go
@@ -1,43 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-
-	"github.com/Adosh74/geek-blog/config"
-	"github.com/gin-gonic/gin"
-	"github.com/spf13/viper"
+	"github.com/Adosh74/geek-blog/cmd"
 )
 
 func main() {
-
-	configs := configSet()
-
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message":  "pong",
-			"app name": viper.Get("App.Name"),
-		})
-	})
-	r.Run(fmt.Sprintf("%s:%s", configs.Server.Host, configs.Server.Port))
-}
-
-func configSet() config.Config {
-	viper.SetConfigName("config")
-	viper.SetConfigType("yaml")
-	viper.AddConfigPath("config")
-
-	if err := viper.ReadInConfig(); err != nil {
-		fmt.Printf("Error reading config file, %s", err)
-	}
-
-	var configs config.Config
-
-	err := viper.Unmarshal(&configs)
-	if err != nil {
-		fmt.Printf("unable to decode into struct, %v", err)
-	}
-
-	return configs
+	cmd.Execute()
 }
