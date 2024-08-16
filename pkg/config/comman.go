cea4a2d2fd80c2765df8687f9577f5630dd6d56e
Mohamed Shebl <hamdysheblm@gmail.com>
2024-08-16 14:37:58 +0300

    create config package create setter and getter to config package

---
+++ pkg/config/comman.go
@@ -0,0 +1,5 @@
+package config
+
+import "github.com/Adosh74/geek-blog/config"
+
+var configurations config.Config
---
+++ pkg/config/getter.go
@@ -0,0 +1,7 @@
+package config
+
+import "github.com/Adosh74/geek-blog/config"
+
+func Get() config.Config {
+	return configurations
+}
