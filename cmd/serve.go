eec6607bc09d2d0393fbf3fce9b0b5325a354678
Mohamed Shebl <hamdysheblm@gmail.com>
2024-08-16 14:42:24 +0300

    update serving after creating config and routing packages

--- cmd/serve.go
+++ cmd/serve.go
@@ -1,10 +1,10 @@
 package cmd
 
 import (
-	"fmt"
 	"net/http"
 
-	"github.com/Adosh74/geek-blog/config"
+	"github.com/Adosh74/geek-blog/pkg/config"
+	"github.com/Adosh74/geek-blog/pkg/routing"
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -25,33 +25,15 @@ var serveCmd = &cobra.Command{
 
 func serve() {
 
-	configs := configSet()
+	config.Set()
 
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	router := routing.GetRouter()
+
+	router.GET("/healthz", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message":  "pong",
+			"message":  "OK",
 			"app name": viper.Get("App.Name"),
 		})
 	})
-	r.Run(fmt.Sprintf("%s:%s", configs.Server.Host, configs.Server.Port))
-}
-
-func configSet() config.Config {
-	viper.SetConfigName("config")
-	viper.SetConfigType("yaml")
-	viper.AddConfigPath("config")
-
-	if err := viper.ReadInConfig(); err != nil {
-		fmt.Printf("Error reading config file, %s", err)
-	}
-
-	var configs config.Config
-
-	err := viper.Unmarshal(&configs)
-	if err != nil {
-		fmt.Printf("unable to decode into struct, %v", err)
-	}
-
-	return configs
+	routing.Serve()
 }
