ba5a06c079d1af5a156b63c5a3fdd4eec455e7ce
Mohamed Shebl <hamdysheblm@gmail.com>
2024-08-17 14:48:00 +0300

    complete clean base code architecture

--- internal/modules/home/routes/routes.go
+++ internal/modules/home/routes/routes.go
@@ -3,21 +3,27 @@ package routes
 import (
 	"net/http"
 
+	"github.com/Adosh74/geek-blog/pkg/html"
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
 )
 
 func Routes(router *gin.Engine) {
 	router.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message":  "Home",
-			"app name": viper.Get("App.Name"),
+		html.Render(c, http.StatusOK, "modules/home/html/home", gin.H{
+			"title": "Home page",
 		})
 	})
+	router.GET("/about", func(c *gin.Context) {
+		html.Render(c, http.StatusOK, "modules/home/html/about", gin.H{
+			"title": "About page",
+		})
+	})
+
 	router.GET("/healthz", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message":  "OK",
-			"app name": viper.Get("App.Name"),
+			"APP_NAME": viper.Get("App.Name"),
 		})
 	})
 }
--- pkg/bootstrap/serve.go
+++ pkg/bootstrap/serve.go
@@ -2,13 +2,19 @@ package bootstrap
 
 import (
 	"github.com/Adosh74/geek-blog/pkg/config"
+	"github.com/Adosh74/geek-blog/pkg/html"
 	"github.com/Adosh74/geek-blog/pkg/routing"
+	"github.com/Adosh74/geek-blog/pkg/static"
 )
 
 func Serve() {
 	config.Set()
 
 	routing.Init()
+
+	static.LoadStatic(routing.GetRouter())
+
+	html.LoadHTML(routing.GetRouter())
 
 	routing.RegisterRoute()
 
